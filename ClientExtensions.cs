c01df3b5b959fbf33714108c9bc8c83cfc89aa8c
Brian Bosak <brielcccc@gmail.com>
2016-07-03 08:59:52 -0500

    Added support for serializing arbitrary data types easily.

--- ClientExtensions.cs
+++ ClientExtensions.cs
@@ -8,5 +8,21 @@ namespace IDWDBClient
 {
     public static class ClientExtensions
     {
+        public static TableQuery InsertOrReplace(this TableQuery query, object value)
+        {
+            var pk = value.GetType().GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public).Where(m => m.CustomAttributes.Where(a => a.AttributeType == typeof(System.ComponentModel.DataAnnotations.KeyAttribute)).Any());
+            if (!pk.Any())
+            {
+                throw new Exception("No primary key specified.");
+            }
+            DataRow boat = new DataRow(pk.First().GetValue(value));
+
+            foreach (var iable in value.GetType().GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public))
+            {
+                boat.AddColumn(iable.Name, iable.GetValue(value));
+            }
+            return query.InsertOrReplace(boat);
+
+        }
     }
 }
